80dbbcf58558714104bd2528f046909b1c930291
Yuri Azevedo <yuriazevedo11@gmail.com>
2019-06-30 15:10:00 -0300

    NEW - Rota para criação de um novo projeto

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,18 @@
 const express = require('express')
 
 const routes = new express.Router()
-const projects = [{ id: '1', title: 'Exemplo projeto', tasks: ['Exemplo tarefa'] }]
+const projects = []
 
 routes.get('/projects', (req, res) => {
   return res.json(projects)
+})
+
+routes.post('/projects', (req, res) => {
+  const { id, title } = req.body
+
+  projects.push({ id, title, tasks: [] })
+
+  return res.status(201).json(projects)
 })
 
 module.exports = routes
